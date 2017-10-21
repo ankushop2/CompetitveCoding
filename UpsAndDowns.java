82fa7c5158e8077feed73107170126b7f952ed4e
Ankush Arunkumar <ankush1997@gmail.com>
2017-10-21 11:06:08 +0530

    Updates

--- UpsAndDowns.java
+++ UpsAndDowns.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.*;
 import java.io.*;
-
+//Java is thr s c e n e
 class  UpsAndDowns {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
